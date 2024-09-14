16c202d803cc4487310de3fce84a6d07c13881c7
Nikolas Gustavson <140442591+thefinnishstranger@users.noreply.github.com>
2024-09-14 12:36:13 -0500

    Fixed up the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,76 @@
+import { IconButton } from '@mui/material'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import XIcon from '@mui/icons-material/X';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
+const socialIcons = [
+  {
+    link: 'https://instagram.com/nikolasgustavson',
+    icon: InstagramIcon
+  },
+  {
+    link: 'https://github.com/thefinnishstranger',
+    icon: GitHubIcon
+  },
+  {
+    link: 'https://x.com/nikgustavson',
+    icon: XIcon
+  },
+  {
+    link: 'https://www.facebook.com/nikolas.gustavson',
+    icon: FacebookIcon
+  },
+  {
+    link: 'https://www.youtube.com/@nikolasgustavson2493',
+    icon: YouTubeIcon
+  },
+]
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center bg-green-500 text-white p-20">
-      <div className="grid grid-cols-4">
-        <div className="p-10">
-          <a href="/">
+    <footer className="bg-green-500 text-white py-20 text-center tracking-tight">
+      <div className="">
+        <div className="flex justify-center">
+          <a href="/" className="transition transform hover:text-slate-200 duration-150 mr-6 ml-6">
             Home
           </a>
-        </div>
-        <div className="p-10">
-          <a href="/calculator">
+          <a href="/calculator" className="transition transform hover:text-slate-200 duration-150 mr-6 ml-6">
             Budget Calculator
           </a>
-        </div>
-        <div className="p-10">
-          <a href="/account">
+          <a href="/account" className="transition transform hover:text-slate-200 duration-150 mr-6 ml-6">
             Dashboard
           </a>
-        </div>
-        <div className="p-10">
-          <a href="/about">
+          <a href="/about" className="transition transform hover:text-slate-200 duration-150 mr-6 ml-6">
             About us
           </a>
         </div>
+        <div className="">
+        </div>
+        <div className="">
+          
+        </div>
+        <div className="">
+          
+        </div>
+      </div>
+      <div>
+        {socialIcons.map((social) => (
+          <IconButton sx={{
+            ml: 1,
+            "&.MuiButtonBase-root:hover": {
+              bgcolor: "transparent"
+            }
+            }}>
+            <a href={social.link} target='_blank' rel='noopener noreferrer' className='text-white m-4 transition transform hover:text-slate-200 duration-150'>
+            <social.icon />
+            </a>
+          </IconButton>
+        ))}
+      </div>
+      <div className="text-sm font-thin mt-1">
+        © 2024 Budget, Inc. All rights reserved.
       </div>
     </footer>
   )
