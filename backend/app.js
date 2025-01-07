b71711802ce707bfb842a9eb7fa65beed53d4ba9
Nikolas Gustavson <140442591+thefinnishstranger@users.noreply.github.com>
2025-01-06 18:48:37 -0600

    add

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,12 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
     app.use(cors());
     
-   
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get("authorization");
